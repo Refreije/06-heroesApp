db4aacdff5d21c1292a6f234de1408a9ba2b16f2
hrequeijo <hrequeijo@intermarkit.es>
2025-05-19 16:45:59 +0200

    crud 1

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,8 +1,10 @@
+import { observable } from './../../../../node_modules/rxjs/src/internal/symbol/observable';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/envirnments';
+import { query } from '@angular/animations';
 
 
 @Injectable({providedIn: 'root'})
@@ -17,18 +19,36 @@ export class HeroesService {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
   }
 
-getHeroById(id: string): Observable<Hero|undefined>
-{
-  return  this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
-  .pipe(
-    catchError( error =>  of(undefined))
-  )//El of crea un observable con el valor que hay en los parentesis. O sea crea un observable que devuelve undefined
-}
+  getHeroById(id: string): Observable<Hero|undefined>
+  {
+    return  this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError( error =>  of(undefined))
+    )//El of crea un observable con el valor que hay en los parentesis. O sea crea un observable que devuelve undefined
+  }
 
   getSuggestions(query: string): Observable<Hero[]>{
 
 
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if(!hero.id) throw Error('hero id is requiered')
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError( err => of(false)),
+      map( resp => true)
+    );
   }
 
 
